1f9f38501c4dc00abb8ba9d3d2aa7ebda6c26382
Anubhi <anubhi9k@gmail.com>
2021-05-18 19:20:16 +0530

    Largest prime factor of the number 600851475143

--- ProjectEuler/3.php
+++ ProjectEuler/3.php
@@ -1,19 +1,16 @@
 <?php
  function LargestPrimeFactor($number){
     $x = 2;
-    
-    while($x <= $number){
-        $quotient= $number/$x;
-        if($quotient == 1){
-            echo $number;
-            break;
-         }
-        elseif(is_int($quotient) == true){
-            $number = $number/$x; 
-        } else {
-           $x++;
+     while($x < $number){  //until our iterator hits the number       
+        $quotient= $number/$x; //check if its perfect division
+         if(is_int($quotient) == true){ //if its whole number
+            $number = $number/$x;  // divide the number
         } 
-        }
+        $x++; //else increment 
+    }
+    return $x; //return the number since in this break condition it's been 
+    //found to be the LPF
     }
     echo LargestPrimeFactor(600851475143);
+    //6857
 ?>
