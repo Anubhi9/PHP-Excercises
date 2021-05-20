ccfdfb8d1aadd2fa1967bc27e498b341b56cd3d9
Anubhi <anubhi9k@gmail.com>
2021-05-21 01:03:35 +0530

    Faster version of Pythagorean Triplet problem

--- ProjectEuler/9.php
+++ ProjectEuler/9.php
@@ -2,16 +2,11 @@
     $start = microtime(true);
 for($a = 1; $a < 500; $a++){
     for($b = 1; $b < 500; $b++){
-        for($c = (1000 - ($a + $b)); $c < ($a + $b); $c++){
-            if(pow($a,2) + pow($b,2) == pow($c,2)){ //check sum of squares of first two numbers
-                if($a + $b + $c == 1000){ //check sum of all 3
-                    echo $a*$b*$c; 
-                    break;
-                }
+         $c = 1000 - ($a + $b);
+            if(pow($a,2) + pow($b,2) == pow($c,2))//check sum of squares of first two numbers
+                    echo $a*$b*$c . "\n"; 
             }
         }
-    }
-}
 $time_elapsed_secs = microtime(true) - $start;
 echo $time_elapsed_secs;
 
